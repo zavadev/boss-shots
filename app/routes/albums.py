1bfb1b8060408623203a5816e6e37a93fc9a86d6
Kateryna <tsymbal.katena@mail.ru>
2022-05-02 13:51:49 -0500

    Update finished and andded csrf token for add new Album

--- app/routes/albums.py
+++ app/routes/albums.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request
 from app.models.db import db
 from app.models.album import Album
 from app.forms.add_album_form import AlbumForm
@@ -20,8 +20,8 @@ def add_album():
   user_id = current_user.id
   form = AlbumForm()
 
-  print("&&&&&&&=====>>>>>>>>>", form.errors)
 
+  form['csrf_token'].data = request.cookies['csrf_token']
   if form.validate_on_submit():
     new_album = Album(
       user_id = user_id,
@@ -30,7 +30,7 @@ def add_album():
     print("======>>>>>>>", new_album)
     db.session.add(new_album)
     db.session.commit()
-    return redirect("/api/albums/all")
+    return  new_album.to_dict()
 
   return render_template("test.html", form=form)
 
@@ -45,9 +45,10 @@ def single_album(id):
 def update_album(id):
   album = Album.query.get(id)
   form = AlbumForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
   if form.validate_on_submit():
     album.title = form.data["title"]
     db.session.commit()
-    return redirect("/{id}")
+    return album.to_dict()
 
   return render_template("test.html", form=form)
