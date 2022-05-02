95f685011bfca3eeb23e4800a4532caca3c57a11
Crystal <crystal120599@gmail.com>
2022-05-01 18:10:33 -0700

    favorite model

--- app/models/favorite.py
+++ app/models/favorite.py
@@ -2,3 +2,17 @@ from .db import db
 
 class Favorite(db.Model):
     __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer(), nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'),nullable=False)
+    photo_id = db.Column(db.Integer(), db.ForeignKey('photos.id'),nullable=False)
+
+    user = db.relationship('User',back_populates='favorites')
+    photo = db.relationship('Photo',back_populates='favorites')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'photo_id': self.photo_id,
+        }
