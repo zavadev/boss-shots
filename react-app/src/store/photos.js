942c6b456ddff72f595efef0b0884b10525dea63
Nathan Zavala <zavalanathana@gmail.com>
2022-05-03 10:20:58 -0700

    Post photo thunk

--- react-app/src/store/photos.js
+++ react-app/src/store/photos.js
@@ -1,8 +1,14 @@
 const GET_PHOTOS = 'photos/GET_PHOTOS'
+const POST_PHOTO = 'photos/POST_PHOTO'
 
 const getAllPhotos = (photos) => ({
   type: GET_PHOTOS,
   payload: photos
+})
+
+const postPhoto = (photo) => ({
+  type: POST_PHOTO,
+  payload: photo
 })
 
 const getAllPhotosThunk = () => async (dispatch) => {
@@ -15,6 +21,21 @@ const getAllPhotosThunk = () => async (dispatch) => {
   return response
 }
 
+const postPhotoThunk = (photo) => async (dispatch) => {
+  const response = await fetch('/api/photos/add_photo', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(photo)
+  })
+
+  if (response.ok) {
+    const newPhoto = await response.json();
+    dispatch(postPhoto(newPhoto));
+    return newPhoto;
+  }
+  return response;
+}
+
 
 const initialState = {};
 
@@ -24,6 +45,9 @@ const photosReducer =  (state = initialState, action) => {
     case GET_PHOTOS:
       newState = { ...state }
       action.payload.photos.forEach(photo => newState[photo.id] = photo);
+      return newState;
+    case POST_PHOTO:
+      newState = { [action.photo.id]: action.photo, ...state };
       return newState;
     default:
       return state;
